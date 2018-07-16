8e18919eff37ce7b37e9df3ae07bb981c94c10cc
lunaroyster <lunaroyster@users.noreply.github.com>
2018-07-16 15:39:22 +0000

    initialize action upon bootstrap

--- src/main.js
+++ src/main.js
@@ -2,18 +2,20 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store';
-import AsyncComputed from 'vue-async-computed'
+import AsyncComputed from 'vue-async-computed';
 
 Vue.config.productionTip = false;
 
 Vue.use(AsyncComputed);
 
-store.dispatch('initialize');
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App),
-});
+(async ()=> {
+  await store.dispatch('initialize');
+  
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    render: h => h(App),
+  });
+})();
