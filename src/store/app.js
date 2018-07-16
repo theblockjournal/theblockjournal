4922b27dbe16b7f2de1f25f7429939591fc45d0b
lunaroyster <lunaroyster@users.noreply.github.com>
2018-07-16 15:38:17 +0000

    Accounts state

--- src/store/app.js
+++ src/store/app.js
@@ -3,6 +3,7 @@ import Eth from '@/services/getWeb3';
 export default {
   state: {
     networkID: null,
+    accounts: [],
   },
   getters: {
     networkName(state) {
@@ -19,15 +20,23 @@ export default {
   mutations: {
     setNetworkID(state, id) {
       state.networkID = id;
-    },  
+    },
+    setAccounts(state, accounts) {
+      state.accounts = accounts;
+    }
   },
   actions: {
     async setNetworkID({commit}) {
       let networkID = await Eth.net_version();
       commit('setNetworkID', networkID);
     },
+    async setAccounts({commit}) {
+      let accounts = await Eth.accounts();
+      commit('setAccounts', accounts);
+    },
     async initialize({commit, dispatch}) {
       await dispatch('setNetworkID');
+      await dispatch('setAccounts');
     }
   },
 };
