0ae9cc2933fc185b21ce37131df1d6d7ab747e06
lunaroyster <lunaroyster@users.noreply.github.com>
2018-07-19 03:56:49 +0000

    Tooltip directive

---
+++ src/directives/tooltip.js
@@ -0,0 +1,9 @@
+const $ = window.jQuery;
+
+export default function(el, binding) {
+    $(el).tooltip({
+        title: binding.value,
+        placement: binding.arg,
+        trigger: 'hover',
+    });
+}
--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App';
 import router from './router';
 import store from './store';
 import AsyncComputed from 'vue-async-computed';
+import tooltipDirective from '@/directives/tooltip';
 
 Vue.config.productionTip = false;
 
 Vue.use(AsyncComputed);
+
+Vue.directive('tooltip', tooltipDirective);
 
 (async ()=> {
   await store.dispatch('initialize');
