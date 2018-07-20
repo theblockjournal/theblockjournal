49b097281d540e3b4a99a074fde613a06683ac0f
lunaroyster <lunaroyster@users.noreply.github.com>
2018-07-20 13:40:43 +0000

    Sign page layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,8 @@
     <div class="container-fluid p-0">
       <div class="row m-0">
         <Sidebar class="col-3 p-0"/>
-        <Document class="col-9 p-0" ref="document"/>
+        <Document class="col-9 p-0" ref="document" v-if="!sign"/>
+        <Sign class="col-9 p-0" v-if="sign"/>
       </div>
     </div>
   </div>
@@ -14,6 +15,7 @@
 import Menu from '@/components/Menu';
 import Sidebar from '@/components/Sidebar';
 import Document from '@/components/Document';
+import Sign from '@/components/Sign';
 
 import Eth from '@/services/getWeb3';
 import blockJournal from '@/services/blockJournal';
@@ -26,6 +28,12 @@ export default {
     Menu,
     Sidebar,
     Document,
+    Sign,
+  },
+  computed: {
+    sign() {
+      return this.$store.getters.currentSign;
+    },
   },
   data() {
     return {};
---
+++ src/components/Sign.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="sign">
+    <div id="signView" class="container p-4">
+      <h3>Filename <span class="text-muted">v0</span></h3>
+      <div class="card border-0 my-2">
+        <div class="card-body">
+          <h5 class="card-title">Verification</h5>
+          <i class="material-icons">check_circle_outline</i>
+        </div>
+      </div>
+      <div class="card border-0 my-2">
+        <div class="card-body">
+          <h5 class="card-title">Signature details</h5>
+          <p class="card-text">Retrieved from local storage</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item d-flex">
+            <i class="material-icons px-2">fingerprint</i>
+            <span class="text-muted">{{sign.signature}}</span>
+          </li>
+          <li class="list-group-item d-flex">
+            <i class="material-icons px-2">account_circle</i>
+            <span class="text-muted">{{sign.sender}}</span>
+          </li>
+          <li class="list-group-item d-flex">
+            <i class="material-icons px-2">access_time</i>
+            <span class="text-muted">{{getTimeString(sign.time)}}</span>
+          </li>
+          <li class="list-group-item d-flex">
+            <i class="material-icons px-2">bookmark</i>
+            <span class="text-muted">{{sign.txHash}}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+import blockJournal from '@/services/blockJournal';
+
+export default {
+  name: 'Sign',
+  data() {
+    return {};
+  },
+  computed: {
+    sign() {
+      return this.$store.getters.currentSign;
+    },
+  },
+  methods: {
+    getTimeString: (timestamp)=> {
+      return moment(timestamp).toString();
+    },
+  }
+};
+</script>
+
+<style scoped lang="scss">
+#sign {
+  background: #eeeeee;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  top: 4em;
+}
+</style>
