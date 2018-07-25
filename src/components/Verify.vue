0a070a49debb074690a1bf728b4fbac1c6f4c891
lunaroyster <lunaroyster@users.noreply.github.com>
2018-07-25 08:24:42 +0000

    Verify component

---
+++ src/components/Verify.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="verify">
+    <div class="container">
+      <div class="card border-0 my-2">
+        <div class="card-body">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Verify',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="scss">
+#verify {
+  background-color: #eeeeee;
+  position: absolute;
+  top: 4em;
+  bottom: 0;
+  width: 100%;
+}
+</style>
