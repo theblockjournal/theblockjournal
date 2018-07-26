473d4328e6482003a99e3254976aaa688839b20d
lunaroyster <lunaroyster@users.noreply.github.com>
2018-07-26 15:28:05 +0000

    deployment (1)

---
+++ src/contracts/v0/index.js
@@ -0,0 +1,8 @@
+import * as blockJournalABI from './blockJournal.abi.json';
+
+export default {
+    abi: blockJournalABI,
+    deployments: {
+        '4': '0x32e30615fdf4631259644af2305e2317f5b28a0e',
+    },
+};
