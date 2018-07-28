c8cab3ac2d62c77b219265f56a0ab693a5ed3fd2
lunaroyster <lunaroyster@users.noreply.github.com>
2018-07-28 12:30:03 +0000

    Added infura httpProvider (enables verification without metamask)

--- src/services/getEthjs.js
+++ src/services/getEthjs.js
@@ -1,7 +1,14 @@
 import Web3 from 'web3';
 import Eth from 'ethjs';
 
-const eth = new Eth(window.web3.currentProvider);
+let eth;
+
+if (typeof window.web3 !== 'undefined' && typeof window.web3.currentProvider !== 'undefined') {
+  eth = new Eth(window.web3.currentProvider);
+} else {
+  let provider = new Eth.HttpProvider('https://rinkeby.infura.io/v3/220fd5019950440f8be4d303eb3a74e6');
+  eth = new Eth(provider);
+}
 
 window.Eth = eth;
 
